d41f0377dab23e55ff3db48dab3b5f3244815cbf
animagascar <anima.gascar@gmail.com>
2023-01-20 13:19:56 +0300

    mis tous

---
+++ News.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style6.css" type="text/css">
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/all.css">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.css">
+    <title>News</title>
+</head>
+<body>
+<header>
+    <?php
+    include "header.php";
+    ?>   
+    </header>
+    <div class="containeur" id="ct">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>News</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct1">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Articles des voyages</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia similique nulla aut? Maxime iure aut nemo accusamus nulla eligendi <br> necessitatibus praesentium facere! Similique distinctio molestiae accusamus consequuntur praesentium alias beatae.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct2">
+        <div class="row">
+            <div class="col-lg-12">
+                <input type="button" value="Voyage d’aventure" id="btn1">
+                <input type="button" value="Explorez le monde"id="btn2">
+                <input type="button" value="Vacances en famille"id="btn3">
+                <input type="button" value="Art et culture"id="btn4">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct3">
+        <div class="row" id="row1">
+            <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24.png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (1).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (2).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+        <div class="row"id="row2">
+        <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (3).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (4).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (5).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+        <div class="row"id="row3">
+        <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (6).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (7).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (8).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct4">
+    <div class="row" id="row1">
+            <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (25).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (9).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (10).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+        <div class="row"id="row2">
+        <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (11).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (12).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (13).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+        <div class="row"id="row3">
+        <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (14).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (26).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct5">
+    <div class="row" id="row1">
+            <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (15).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (16).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (17).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+        <div class="row"id="row2">
+        <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (18).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (19).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct6">
+    <div class="row" id="row1">
+            <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (20).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (21).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (22).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+        </div>
+        <div class="row"id="row2">
+        <div class="col-lg-4">
+                <img src="NewsIMG/Rectangle 24 (23).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            <img src="NewsIMG/Rectangle 24 (24).png" alt="img"><br><br>
+                <span>Lorem ipsum dolor sit amet, aliquam,<br> purus.</span><br>
+                <samp>19 Avril 2023</samp>
+            </div>
+            <div class="col-lg-4">
+            </div>
+        </div>
+    </div>
+    <?php
+        include"footer.php";
+    ?>
+    </footer>
+</body>
+</html>
---
+++ Propos.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style7.css" type="text/css">
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/all.css">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.css">
+    <title>A Propos</title>
+</head>
+<body>
+<header>
+    <?php
+    include "header.php";
+    ?>   
+    </header>
+    <div class="containeur" id="ct1">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>A propos</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct2">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Pourquoi nous choisir ?</h1>
+                <p>Ces destinations populaires ont beaucoup à offrir</p>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct3">
+        <div class="row">
+            <div class="col-lg-8" id="lg8">
+                <h1>À propos de Fosa travel</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dictum et in feugiat. Platea in diam, est congue. Posuere sapien morbi augue ultrices. Et facilisi orci sollicitudin placerat lacus lacus nibh. Egestas semper massa viverra massa proin in morbi placerat. Pharetra nec, est non integer nisi, ut faucibus. Non, in nam sollicitudin vitae volutpat ac molestie. Turpis pellentesque sit pellentesque id cras lobortis tortor, blandit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet dictum et in feugiat. Platea in diam, est congue. Posuere sapien morbi augue ultrices. Et facilisi orci sollicitudin placerat lacus lacus nibh. Egestas semper massa viverra massa proin in morbi placerat. Pharetra nec, est non integer nisi, ut faucibus. Non, in nam sollicitudin vitae volutpat ac molestie. Turpis pellentesque sit pellentesque id cras lobortis tortor, blandit.</p>
+            </div>
+            <div class="col-lg-4">
+                <img src="ProposIMG/image 6.png" alt="img">
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct4">
+        <div class="row">
+            <h1>Nos équipes</h1>
+            <p>Lorem ipsum dolor sit amet</p>
+            <div class="col-lg-5">
+                <div class="containeur" id="ct4_1">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <img src="ProposIMG/Rectangle 30.png" alt="img">
+                            <h2>Agung perdana</h2>
+                            <span>comptables </span>
+                        </div>
+                        <div class="col-md-6">
+                        <img src="ProposIMG/Rectangle 30 (1).png" alt="img">
+                        <h2>Rahardianysah</h2>
+                        <span>Guides touristiques </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="containeur" id="ct4_2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="ProposIMG/Rectangle 30 (2).png" alt="img">
+                            <h2>Viola Resti</h2>
+                            <span>Gèrent </span>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="ProposIMG/Rectangle 30 (3).png" alt="img">
+                            <h2>Andika</h2>
+                            <span>Gèrents </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2">
+            <img src="ProposIMG/Rectangle 30 (4).png" alt="img">
+                <h2>Ikram shoud</h2>
+                <span>Gèrent </span>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="ct5">
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="ProposIMG/Vector.svg" alt="svg"><br><br>
+                <h4>Meilleur prix</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+            <div class="col-lg-4">
+                <img src="ProposIMG/Vector (2).svg" alt="svg"><br><br>
+                <h4>Réservation facile</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+            <div class="col-lg-4">
+                <img src="ProposIMG/Vector (1).svg" alt="svg"><br><br>
+                <h4>Service à la clientèle 24h/24</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+        </div>
+    </div>
+    <footer>
+    <?php
+        include"footer.php";
+    ?>
+    </footer>
+</body>
+</html>
